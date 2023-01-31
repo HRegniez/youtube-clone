ad0d6a74d05e78481862978ec87e2d1811593f8c
HRegniez <henriregniez124@gmail.com>
2023-01-31 12:02:11 +0100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 
 import { logo } from '../utils/constants';
 
+import SearchBar from './SearchBar';
+
 const Navbar = () => (
   <Stack 
     direction="row" 
@@ -14,6 +16,8 @@ const Navbar = () => (
     <Link to='/' style={{ display: 'flex', alignItems: 'center'}}>
       <img src={logo} alt="logo" height={45} />
     </Link>
+
+    <SearchBar />
 
   </Stack>
 )
